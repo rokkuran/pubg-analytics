71a2c98441e2eeed0ee48381c94acfdfba0a7efa
Unknown <lachlanbtaylor@gmail.com>
2018-07-06 12:28:02 +1000

    quick and dirty refactor

--- generate.jl
+++ generate.jl
@@ -35,41 +35,38 @@ function team_maps(df::DataFrame)
 end
 
 
-function main()
-  match_id = "64c6a84d-49b4-4c6c-943e-26bc8676b611"
-  filename = "pubg_kill_events_$(match_id).csv"
+get_players(df::DataFrame) = union(df[:killer], df[:victim])
 
-  path_kill_events = "c:/workspace/pubg-analytics/output/$filename"
 
-  df = CSV.read(path_kill_events)
-
-  println(tail(df))
-
-  # TODO: refactor out to functions
-  killer_team_map = Dict(zip(df[:killer], df[:killer_team_id]))
-  victim_team_map = Dict(zip(df[:victim], df[:victim_team_id]))
-  # killer_team_map, victim_team_map = team_maps(df)
-
-  player_team_map = merge(killer_team_map, victim_team_map)
-
-  players = union(df[:killer], df[:victim])
-
-  id_player_map = create_id_nodename_map(players)
+function player_id_maps(df::DataFrame)
+  id_player_map = create_id_nodename_map(get_players(df))
   player_id_map = map(reverse, id_player_map)
+  id_player_map, player_id_map
+end
 
-  # TODO: refactor into build_graph function
+
+function build_graph(df::DataFrame)
+
   node_edge_map = zip(df[:killer], df[:victim])
+  id_player_map, player_id_map = player_id_maps(df)
 
   G = DiGraph(length(id_player_map))
   add_edges!(G, node_edge_map, player_id_map)
+  
+  G
+end
 
-  # refactor into graph properties function
+function print_properties(G::AbstractGraph)
   println("is_directed: $(is_directed(G))")
   println("n_vetrices: $(nv(G))")
   println("n_edges: $(ne(G))")
   println("has_self_loops: $(has_self_loops(G))")
+end
 
-  # TODO: refactor
+
+function player_kill_counts(df)
+  players = get_players(df)
+
   player_kills = Dict{String, Float64}()
   for player in players
     player_kills[player] = 0
@@ -79,17 +76,18 @@ function main()
     player_kills[k] += 1
   end
 
-  # TODO: refactor
-  n_teams = maximum(values(player_team_map))# + 1  # for non-killer caused deaths
-  println("n_teams: $n_teams")
+  player_kills
+end
 
-  team_colours = get(ColorSchemes.rainbow, collect(linspace(0, 1, n_teams)))
-  
-  # TODO: refactor
+
+team_colour_map(n_teams) = get(ColorSchemes.rainbow, collect(linspace(0, 1, n_teams)))
+
+
+function player_team_colour_map(players, player_team_map, team_colours)
+
   player_team_colour = Dict()
   for player in players
     team_id = player_team_map[player]
-    println(team_id)
     if team_id == 0
       player_team_colour[player] = RGB{Float64}(128/256, 128/256, 128/256)
     else
@@ -97,27 +95,63 @@ function main()
     end
   end
 
-  # TODO: refactor
+  player_team_colour
+end
+
+
+function generate_graph_plot(G::AbstractGraph, player_kills, id_player_map, player_team_colour, output::String)
+
   srand(312132)
   loc_x, loc_y = spring_layout(G)
 
   node_sizes = [player_kills[id_player_map[i]] + 1 for i in 1:nv(G)]  # add one to offset zero sized nodes
   node_sizes .^= 0.3  # zoom factor
 
-  filename = "pubg_kill_graph_$match_id"
-
   draw(
-    PNG("c:/workspace/pubg-analytics/output/$filename.png", (1920 - 1920 * 0.1)*2px, (1080 - 1080 * 0.1)*2px),
+    PNG(output, (1920 - 1920 * 0.1)*2px, (1080 - 1080 * 0.1)*2px),
     gplot(
       G, 
       nodelabel=[id_player_map[i] for i in 1:nv(G)],
       nodesize=node_sizes,
-#         nodelabelsize=node_sizes,
+      # nodelabelsize=node_sizes,
       nodefillc=[player_team_colour[id_player_map[i]] for i in 1:nv(G)],
       arrowlengthfrac=0.02,
       loc_x, loc_y
     ),
   )
+
+end
+
+
+
+function main()
+  match_id = "64c6a84d-49b4-4c6c-943e-26bc8676b611"
+  filename = "pubg_kill_events_$(match_id).csv"
+
+  path_kill_events = "c:/workspace/pubg-analytics/output/$filename"
+
+  df = CSV.read(path_kill_events)
+  println(tail(df))
+
+  killer_team_map, victim_team_map = team_maps(df)
+  player_team_map = merge(killer_team_map, victim_team_map)
+
+  players = get_players(df)
+  id_player_map, player_id_map = player_id_maps(df)
+
+  G = build_graph(df)
+  print_properties(G)
+
+  player_kills = player_kill_counts(df)
+
+  n_teams = maximum(values(player_team_map))  # + 1  # for non-killer caused deaths
+  team_colours = team_colour_map(n_teams)
+  
+  player_team_colour = player_team_colour_map(players, player_team_map, team_colours)
+
+
+  output_path = "c:/workspace/pubg-analytics/output/pubg_kill_graph_$match_id.png"
+  generate_graph_plot(G, player_kills, id_player_map, player_team_colour, output_path)
 
 end
 
